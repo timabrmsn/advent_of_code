7bb03c80905a47614caa977df27fb9341f0a2263
Tim Abramson <tim.abramson@code42.com>
2020-12-02 06:26:54 -0600

    day2, part2

--- day2/p2.py
+++ day2/p2.py
@@ -4,6 +4,6 @@ print(len(
     [
         i for i in
         [re.match(r"(\d+)-(\d+) ([a-z]): ([a-z]+)", line).groups() for line in open("input", "r")]
-        if int(i[0]) <= i[3].count(i[2]) <= int(i[1])
+        if ((i[3][int(i[0])-1] == i[2]) ^ (i[3][int(i[1])-1] == i[2]))
     ]
 ))
