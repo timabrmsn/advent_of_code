84ced8b4409d270e2e8bde4888f3fb6f573ad2eb
Tim Abramson <tim.abramson@code42.com>
2020-12-08 22:10:44 -0600

    day 8 python p1

---
+++ day8/p1.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+with open("input") as file:
+    data = [line.split() for line in file.readlines()]
+
+acc_total = 0
+position = 0
+cmd_counts = defaultdict(lambda: 0)
+
+
+def check_position_only_run_once(position):
+    global acc_total
+    cmd_counts[position] += 1
+    if cmd_counts[position] > 1:
+        print(acc_total)
+        exit()
+        
+
+def jmp(num):
+    global cmd_counts
+    global acc_total
+    global position
+    position += int(num)
+
+
+def acc(num):
+    global cmd_counts
+    global acc_total
+    global position
+    position += 1
+    acc_total += int(num)
+    
+
+def nop(num):
+    global cmd_counts
+    global position
+    position += 1
+
+
+exec_dict = {
+    "acc": acc,
+    "jmp": jmp,
+    "nop": nop,
+}
+
+while True:
+    cmd, num = data[position]
+    check_position_only_run_once(position)
+    exec_dict[cmd](num)
