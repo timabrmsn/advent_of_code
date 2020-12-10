295da5040bb24c141938269c9db9a6fdf1696c0c
Tim Abramson <tim.abramson@code42.com>
2020-12-09 19:04:14 -0600

    day9 python p2

---
+++ day9/p2.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+with open("input") as file:
+    data = [int(line.strip()) for line in file.readlines()]
+
+key = 1309761972
+current = deque()
+
+
+def check_key(c):
+    if sum(c) == key:
+        print(min(c) + max(c))
+        exit()
+
+
+for i in data:
+    current.append(i)
+    check_key(current)
+    if sum(current) > key:
+        while sum(current) >= key:
+            current.popleft()
+            check_key(current)
